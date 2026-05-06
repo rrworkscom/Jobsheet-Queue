67d43bf93b047b46e14f89e9feb607bd40d83c77
rrworkscom <rrworks010@gmail.com>
2026-05-06 20:41:12 +0700

    View Rear menu

---
+++ StudentCourse.java
@@ -0,0 +1,14 @@
+class StudentCourse {
+    String nim, name, studyProgram, className;
+
+    public Student(String nim, String name, String studyProgram, String className) {
+        this.nim = nim;
+        this.name = name;
+        this.studyProgram = studyProgram;
+        this.className = className;
+    }
+
+    void print() {
+        System.out.println(nim + " - " + name + " - " + studyProgram + " - " + className);
+    }
+}
