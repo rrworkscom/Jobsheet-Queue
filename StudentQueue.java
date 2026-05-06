3e0943cce436fea88dcd33e08a1ddfa5b61a8d5f
rrworkscom <rrworks010@gmail.com>
2026-05-06 20:40:32 +0700

    View Rear Added

--- StudentQueue.java
+++ StudentQueue.java
@@ -81,6 +81,15 @@ public class StudentQueue {
         }
     }
 
+    void viewRear() {
+        if (!isEmpty()) {
+            System.out.println("Rear data: ");
+            data[rear].print();
+        } else {
+            System.out.println("Queue is empty!!");
+        }
+    }
+
     void print() {
         if (isEmpty()) {
             System.out.println("Queue is empty!!");
